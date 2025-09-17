2f28b86b80ce1e4e3ecc39dc1c7e1df85cec65e9
russo-leonardo <russo.leonardo@iisgalvanimi.edu.it>
2025-09-17 08:48:22 +0000

    primo commit

---
+++ giorno1.py
@@ -0,0 +1,30 @@
+# Primo blocco
+a = int(input("Dimmi la base: "))
+b = int(input("Dimmi la base: "))
+a = a + b
+print(a)
+
+# Secondo blocco
+a = 5  # numero diverso da zero
+b = 10 # numero diverso da zero
+c = a
+d = b
+c = b
+d = a
+
+# Conversione da Celsius a Kelvin
+f = 273.15
+x = float(input("Inserisci la temperatura in Celsius: "))
+z = x * f
+print(z)
+
+# Terzo blocco
+a = 3
+b = 4
+c = 5
+
+if a > b and c > a:
+    print(a)
+
+if b > a and b < c:
+    print(b)
